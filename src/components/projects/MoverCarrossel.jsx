0bbc3f5b089616eb2dbf9078d0959541683f0ef9
Anderson Willian <anderson2017.willian@gmail.com>
2024-11-06 22:38:16 -0300

    ajuste no efeito da escrita

--- src/components/projects/MoverCarrossel.jsx
+++ src/components/projects/MoverCarrossel.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useCarrossel = (totalItems) => {
     const [mudarIndex, setMudarIndex] = useState(0);
 
+    useEffect(() => {
+        setMudarIndex(0);
+    }, []);  
+
+    
     const moverCarrossel = (direcao) => {
         setMudarIndex((prevIndex) => {
-            const newIndex = prevIndex + direcao;
 
-            if (newIndex < 0) return totalItems - 1;
-            if (newIndex >= totalItems) return 0;
+            let newIndex = prevIndex + direcao;
+
+            if (newIndex < 0) return totalItems - 1;  
+            if (newIndex >= totalItems) return 0;     
 
             return newIndex;
         });
